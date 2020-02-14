de49bcf1d6f880e9cb4e5533209d354a6e2378bf
Hyeok Min Kwon <khmzxc@naver.com>
2020-02-14 17:42:44 +0900

    Init

---
+++ dnf/src/components/Template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Template = ({children}) => {
+    return (
+        <div className="Template">
+            <div className="app-title">아이템 목록</div>
+            <div className="content">{children}</div>
+        </div>
+    );
+};
+
+export default Template;
