ecb7a619096ef06f11e808d1a6f25c6b7be4fdf6
Hyeok Min Kwon <khmzxc@naver.com>
2020-02-18 11:06:14 +0900

    BrowserRouter 적용

--- dnf/src/App.js
+++ dnf/src/App.js
@@ -1,9 +1,18 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+=======
+import React, { useState, useRef } from 'react';
+import Template from './components/Template';
+import List from './components/List';
+import Insert from './components/Insert';
+import { BrowserRouter } from 'react-router-dom';
+>>>>>>> Stashed changes
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +28,15 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+    <div>
+      <BrowserRouter>
+        <Template>
+          <Insert onInsert={onInsert}/>
+          <List items={items}/>
+        </Template>
+      </BrowserRouter>
+>>>>>>> Stashed changes
     </div>
   );
 }
