db7f64849eae94c9cb0636b533a96bc8bba25d80
Hyeok Min Kwon <khmzxc@naver.com>
2020-02-24 13:30:20 +0900

    API 표기

--- dnf/src/App.js
+++ dnf/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import SearchItem from './components/SearchItem';
-import { Route } from 'react-router-dom';
-import ItemDetail from './components/ItemDetail';
-import Menu from './components/Menu';
+import React from "react";
+import SearchItem from "./components/SearchItem";
+import { Route } from "react-router-dom";
+import ItemDetail from "./components/ItemDetail";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <Menu />
-      <Route path="/" component={SearchItem} exact />
-      <Route path="/searchItem" exact component={SearchItem} />
-      <Route path="/searchItem/:itemId" component={ItemDetail} />
+    <div
+      style={{ display: "flex", minHeight: "100vh", flexDirection: "column" }}
+    >
+      <div style={{ flex: "1" }}>
+        <Menu />
+        <Route path="/" component={SearchItem} exact />
+        <Route path="/searchItem" exact component={SearchItem} />
+        <Route path="/searchItem/:itemId" component={ItemDetail} />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ dnf/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../img/기술표기_가로형_color.png";
+
+const Footer = () => {
+  const Powerby = styled.div`
+    display: flex;
+    padding: 1rem;
+    width: 1024px;
+    margin: 0 auto;
+    justify-content: center;
+  `;
+
+  return (
+    <div>
+      <Powerby>
+        <a href="http://developers.neople.co.kr">
+          <img src={logo} alt="Neople 오픈 API" />
+        </a>
+      </Powerby>
+    </div>
+  );
+};
+
+export default Footer;
