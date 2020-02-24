fa1523b40b65be7100357f948f00e66d7f26d9f8
Hyeok Min Kwon <khmzxc@naver.com>
2020-02-24 18:57:57 +0900

    캐릭터 검색 결과 페이지 틀잡기

--- dnf/src/components/SearchCharResult.js
+++ dnf/src/components/SearchCharResult.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const SearchCharResult = () => {
+    /* 제일 위에 "____ 서버에 대한 _____ 검색 결과입니다."
+    서버는 콤보 박스로. 바꾸면 페이지 다시 그리게
+
+    목록에는 캐릭터 이미지와 서버, 이름만 나오게
+    */
+
     return (
         <div>
             
