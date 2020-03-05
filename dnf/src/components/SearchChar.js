8ee3a9d9d499c65d927a67244770e67359b5d94a
Hyeok Min Kwon <khmzxc@naver.com>
2020-03-05 20:45:49 +0900

    쓸데없는 변수를 제거합니다

--- dnf/src/components/SearchChar.js
+++ dnf/src/components/SearchChar.js
@@ -6,7 +6,6 @@ import axios from "axios";
 // 캐릭터 검색 창 (세부 정보x)
 const SearchChar = ({ history }) => {
   const [message, setMessage] = useState("");
-  const [i, setInfo] = useState({});
   const onSearch = name => {
     try {
       axios
@@ -14,7 +13,6 @@ const SearchChar = ({ history }) => {
           `https://api.neople.co.kr/df/servers/all/characters?characterName=${name}&wordType=full&apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
         )
         .then(response => {
-            setInfo(response.data);
           if (response.data.rows.length === 0) {
             setMessage("검색 결과가 없습니다.");
           } else if (response.data.rows.length === 1) {
