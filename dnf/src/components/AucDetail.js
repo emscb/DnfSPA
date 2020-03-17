ee21d495e9c02070ae0720ef4da17c2f7177bd78
Hyeok Min Kwon <khmzxc@naver.com>
2020-03-17 15:35:26 +0900

    경매장 조회 기능 추가

--- dnf/src/components/AucDetail.js
+++ dnf/src/components/AucDetail.js
@@ -35,11 +35,13 @@ const AucDetail = ({ match }) => {
   var table = [
     <thead key="thead">
       <tr>
+        <th></th>
         <th>아이템</th>
         {isStackable && <th>개수 (EA)</th>}
         <th>개당 가격</th>
         <th>총 가격</th>
         <th>어제 평균가</th>
+        <th>남은 시간</th>
       </tr>
     </thead>
   ];
@@ -47,16 +49,32 @@ const AucDetail = ({ match }) => {
     return <div>등록된 매물이 없습니다.</div>;
   } else {
     table.push(
-      <tbody>
+      <tbody key="tbody">
         {list.map(l => (
-          <tr>
+          <tr key={`${l.auctionNo}`}>
             <td>
-              <ItemImg /> {itemInfo.itemName}
+              <ItemImg />
+            </td>
+            <td>
+              <span style={{ margin: "0px 5px" }}>{itemInfo.itemName}</span>
+              {/* 만약 업그레이드가 가능한 카드라면 */}
+              {itemInfo.cardInfo !== undefined &&
+                itemInfo.cardInfo.enchant[0].explain === undefined && (
+                  <span>
+                    {l.upgrade}/{itemInfo.cardInfo.enchant.length - 1}
+                  </span>
+                )}
             </td>
             {isStackable && <td>{l.count}</td>}
-            <td>{l.unitPrice}</td>
-            <td>{l.currentPrice}</td>
-            <td>{l.averagePrice}</td>
+            <td>{l.unitPrice.toLocaleString()}</td>
+            <td>{l.currentPrice.toLocaleString()}</td>
+            <td>{l.averagePrice.toLocaleString()}</td>
+            <td>
+              {parseInt(
+                (Date.parse(l.expireDate) - Date.now()) / 3600000
+              )}
+              시간
+            </td>
           </tr>
         ))}
       </tbody>
