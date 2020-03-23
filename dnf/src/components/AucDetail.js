8b728ad170018ea9962a1c9753dafe034b3405c7
Hyeok Min Kwon <khmzxc@naver.com>
2020-03-23 21:42:02 +0900

    업그레이드에 따라 결과 필터링

--- dnf/src/components/AucDetail.js
+++ dnf/src/components/AucDetail.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Axios from "axios";
 import "./AucDetail.scss";
 import Helmet from "react-helmet";
@@ -9,10 +9,11 @@ const AucDetail = ({ match }) => {
   const gotData = useRef(false);
   const [list, setList] = useState([]);
   const [itemInfo, setItemInfo] = useState({});
+  const [upgrade, setUpgrade] = useState(-1);
 
   if (!gotData.current) {
     Axios.get(
-      `https://api.neople.co.kr/df/auction?itemId=${id}&sort=unitPrice:asc&limit=50&apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
+      `https://api.neople.co.kr/df/auction?itemId=${id}&sort=unitPrice:asc&limit=400&apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
     ).then(response => {
       console.log(response.data.rows);
       setList(response.data.rows);
@@ -37,19 +38,6 @@ const AucDetail = ({ match }) => {
   // 가장 싼 요일, 가장 비싼 요일 계산하기
 
   var isStackable = itemInfo.itemType === "스태커블";
-  var table = [
-    <thead key="thead">
-      <tr>
-        <th></th>
-        <th>아이템</th>
-        {isStackable && <th>개수 (EA)</th>}
-        <th>개당 가격</th>
-        <th>총 가격</th>
-        <th>어제 평균가</th>
-        <th>남은 시간</th>
-      </tr>
-    </thead>
-  ];
   if (list.length === 0) {
     return (
       <div
@@ -59,43 +47,101 @@ const AucDetail = ({ match }) => {
       </div>
     );
   } else {
-    table.push(
-      <tbody key="tbody">
-        {list.map(l => (
-          <tr key={`${l.auctionNo}`}>
-            <td>
-              <ItemImg />
-            </td>
-            <td>
-              <span style={{ margin: "0px 5px" }}>{itemInfo.itemName}</span>
-              {/* 만약 업그레이드가 가능한 카드라면 */}
-              {itemInfo.cardInfo !== undefined &&
-                itemInfo.cardInfo.enchant[0].explain === undefined && (
-                  <span>
-                    {l.upgrade}/{itemInfo.cardInfo.enchant.length - 1}
-                  </span>
-                )}
-            </td>
-            {isStackable && <td>{l.count}</td>}
-            <td>{l.unitPrice.toLocaleString()}</td>
-            <td>{l.currentPrice.toLocaleString()}</td>
-            <td>{l.averagePrice.toLocaleString()}</td>
-            <td>
-              {parseInt((Date.parse(l.expireDate) - Date.now()) / 3600000)}
-              시간
-            </td>
+    const Table = ({ upgrade }) => {
+      var table = [
+        <thead key="thead">
+          <tr>
+            <th></th>
+            <th>아이템</th>
+            {isStackable && <th>개수 (EA)</th>}
+            <th>개당 가격</th>
+            <th>총 가격</th>
+            <th>어제 평균가</th>
+            <th>남은 시간</th>
           </tr>
-        ))}
-      </tbody>
+        </thead>
+      ];
+
+      table.push(
+        <tbody key="tbody">
+          {list.map(
+            l =>
+              (upgrade === -1 ? true : upgrade === l.upgrade) && (
+                <tr key={`${l.auctionNo}`}>
+                  <td>
+                    <ItemImg />
+                  </td>
+                  <td>
+                    <span style={{ margin: "0px 5px" }}>
+                      {itemInfo.itemName}
+                    </span>
+                    {/* 만약 업그레이드가 가능한 카드라면 */}
+                    {itemInfo.cardInfo !== undefined &&
+                      itemInfo.cardInfo.enchant[0].explain === undefined && (
+                        <span>
+                          {l.upgrade}/{itemInfo.cardInfo.enchant.length - 1}
+                        </span>
+                      )}
+                  </td>
+                  {isStackable && <td>{l.count}</td>}
+                  <td>{l.unitPrice.toLocaleString()}</td>
+                  <td>{l.currentPrice.toLocaleString()}</td>
+                  <td>{l.averagePrice.toLocaleString()}</td>
+                  <td>
+                    {parseInt(
+                      (Date.parse(l.expireDate) - Date.now()) / 3600000
+                    )}
+                    시간
+                  </td>
+                </tr>
+              )
+          )}
+        </tbody>
+      );
+      return <table>{table}</table>;
+    };
+
+    const ButtonList = () => {
+      if (
+        itemInfo.cardInfo !== undefined &&
+        itemInfo.cardInfo.enchant[0].explain === undefined
+      ) {
+        let buttonList = [
+          <button key="all" onClick={() => setUpgrade(-1)}>
+            전체
+          </button>
+        ];
+        for (let u = 0; u < itemInfo.cardInfo.enchant.length; u++) {
+          buttonList.push(
+            <button key={u} onClick={() => setUpgrade(u)}>
+              {u}업글
+            </button>
+          );
+        }
+        return <div>{buttonList}</div>;
+      } else {
+        return <div />;
+      }
+    };
+
+    return (
+      itemInfo.itemName !== undefined && (
+        <div>
+          <Helmet>
+            <title>{itemInfo.itemName} 검색 결과</title>
+          </Helmet>
+          {itemInfo.cardInfo !== undefined &&
+            itemInfo.cardInfo.enchant[0].explain === undefined && (
+              <div className="chooseUpgrade">
+                <ButtonList />
+              </div>
+            )}
+          <div className="auclist">
+            <Table upgrade={upgrade} />
+          </div>
+        </div>
+      )
     );
-    return <div>
-      <Helmet>
-        <title>
-          {itemInfo.itemName} 검색 결과
-        </title>
-      </Helmet>
-      <table className="auclist">{table}</table>
-    </div>
   }
 };
 
