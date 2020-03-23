946088f85e7d2375fd02296855882c7a78010372
Hyeok Min Kwon <khmzxc@naver.com>
2020-03-23 20:11:57 +0900

    Remove unused codes

--- dnf/src/components/Home.js
+++ dnf/src/components/Home.js
@@ -1,25 +1,5 @@
 import React from 'react';
 import SearchChar from './SearchChar';
-import axios from "axios";
-import cheerio from "cheerio";
-import iconv from "iconv-lite";
-
-// try {
-//     axios.get(`http://df.nexon.com/df/home`).then(response => {
-//         const $ = cheerio.load(response.data);
-//         console.log(response);
-//         if (response.status === 200) {
-//             const noticeList = $(
-//                 "#container > div.content > div.df_news > ul.news_tabcon > li.show > dl"
-//             );
-//             console.log(noticeList);
-//             var noticeExample = noticeList[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].data;
-//             console.log(iconv.decode(noticeExample, 'CP949').toString());
-//         }
-//     })
-// } catch (e) {
-//     console.log(e);
-// }
 
 const Home = () => {
     return (
