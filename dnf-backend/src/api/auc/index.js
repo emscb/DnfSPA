012e2ae152364b3d8acebdb892f64ae25bad216e
Hyeok Min Kwon <khmzxc@naver.com>
2020-04-20 21:50:24 +0900

    Joi를 이용한 유효성 검사

--- dnf-backend/src/api/auc/index.js
+++ dnf-backend/src/api/auc/index.js
@@ -1,5 +1,6 @@
 import Router from 'koa-router';
 import Auc from '../../models/auc';
+import Joi from 'joi';
 
 const auc = new Router();
 
@@ -11,7 +12,35 @@ GET /auc/:id 아이템 평균판매가 조회
 
 const recentSearch = (ctx) => {};
 
-const avgSave = (ctx) => {};
+const avgSave = async (ctx) => {
+  const schema = Joi.object().keys({
+    date: Joi.date().required(),
+    itemName: Joi.string().required(),
+    itemId: Joi.string().required(),
+    avgPrice: Joi.number().required(),
+  });
+
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400; // Bad request
+    ctx.body = result.error;
+    return;
+  }
+
+  const { date, itemName, itemId, avgPrice } = ctx.request.body;
+  const auc = new Auc({
+    date,
+    itemName,
+    itemId,
+    avgPrice,
+  });
+  try {
+    await auc.save();
+    ctx.body = auc;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 const avgList = (ctx) => {};
 
