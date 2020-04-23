f94eecc61d78dd1172e92a5de0781684bcdbf396
Hyeok Min Kwon <khmzxc@naver.com>
2020-04-23 18:15:58 +0900

    Fix lint

--- dnf/src/components/AucDetail.js
+++ dnf/src/components/AucDetail.js
@@ -5,189 +5,173 @@ import Helmet from "react-helmet";
 import { BACK_URL } from "../config";
 
 var reslist, reslist2;
-const crawl = async (id) => {
-  try {
-    reslist = await Axios.get(
-      `https://api.neople.co.kr/df/auction?itemId=${id}&sort=unitPrice:asc&limit=400&apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
-    );
-    console.log(reslist.data.rows);
+const crawl = async id => {
+	try {
+		reslist = await Axios.get(
+			`https://api.neople.co.kr/df/auction?itemId=${id}&sort=unitPrice:asc&limit=400&apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`,
+		);
+		console.log(reslist.data.rows);
 
-    reslist2 = await Axios.get(
-      `https://api.neople.co.kr/df/items/${id}?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
-    );
-    console.log(reslist2.data);
-  } catch (e) {
-    console.error(e);
-  }
+		reslist2 = await Axios.get(
+			`https://api.neople.co.kr/df/items/${id}?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`,
+		);
+		console.log(reslist2.data);
+	} catch (e) {
+		console.error(e);
+	}
 };
 
 const AucDetail = ({ match, history }) => {
-  const { itemId } = match.params;
-  const id = itemId;
-  const [list, setList] = useState([]);
-  const [itemInfo, setItemInfo] = useState([]);
-  const [upgrade, setUpgrade] = useState(-1);
+	const { itemId } = match.params;
+	const id = itemId;
+	const [list, setList] = useState([]);
+	const [itemInfo, setItemInfo] = useState([]);
+	const [upgrade, setUpgrade] = useState(-1);
 
-  // DB에 데이터 쌓기
-  const date = new Date();
-  let thatday;
-  if (date.getHours() < 6) {
-    thatday = `${date.getFullYear()}-${date.getMonth() + 1}-${
-      date.getDate() - 2
-    }`;
-  } else {
-    thatday = `${date.getFullYear()}-${date.getMonth() + 1}-${
-      date.getDate() - 1
-    }`;
-  }
+	// DB에 데이터 쌓기
+	const date = new Date();
+	let thatday;
+	if (date.getHours() < 6) {
+		thatday = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate() - 2}`;
+	} else {
+		thatday = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate() - 1}`;
+	}
 
-  useEffect(() => {
-    crawl(id).then(() => {
-      if (reslist !== undefined && reslist2 !== undefined) {
-        setList(reslist.data.rows);
-        setItemInfo(reslist2.data);
-      }
-    });
-  }, [id]);
+	useEffect(() => {
+		crawl(id).then(() => {
+			if (reslist !== undefined && reslist2 !== undefined) {
+				setList(reslist.data.rows);
+				setItemInfo(reslist2.data);
+			}
+		});
+	}, [id]);
 
-  useEffect(() => {
-    if (list[0] !== undefined && itemInfo.length !== 0) {
-      try {
-        Axios.post(`${BACK_URL}/auc/${id}`, {
-          date: thatday,
-          itemName: itemInfo.itemName,
-          itemId: itemInfo.itemId,
-          avgPrice: list[0].averagePrice,
-        });
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  }, [list, itemInfo, id, thatday]);
+	useEffect(() => {
+		if (list[0] !== undefined && itemInfo.length !== 0) {
+			try {
+				Axios.post(`${BACK_URL}/auc/${id}`, {
+					date: thatday,
+					itemName: itemInfo.itemName,
+					itemId: itemInfo.itemId,
+					avgPrice: list[0].averagePrice,
+				});
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	}, [list, itemInfo, id, thatday]);
 
-  const ItemImg = () => (
-    <img
-      src={`https://img-api.neople.co.kr/df/items/${itemId}`}
-      alt=""
-      style={{ cursor: "pointer" }}
-      onClick={() => {
-        history.push(`/searchItem/${itemId}`);
-      }}
-    />
-  );
+	const ItemImg = () => (
+		<img
+			src={`https://img-api.neople.co.kr/df/items/${itemId}`}
+			alt=""
+			style={{ cursor: "pointer" }}
+			onClick={() => {
+				history.push(`/searchItem/${itemId}`);
+			}}
+		/>
+	);
 
-  // 그래프 그리기
+	// 그래프 그리기
 
-  // 가장 싼 요일, 가장 비싼 요일 계산하기
+	// 가장 싼 요일, 가장 비싼 요일 계산하기
 
-  var isStackable = itemInfo.itemType === "스태커블";
-  if (list.length === 0) {
-    return (
-      <div
-        style={{ textAlign: "center", fontSize: "1.5rem", fontWeight: "400" }}
-      >
-        등록된 매물이 없습니다.
-      </div>
-    );
-  } else {
-    const Table = ({ upgrade }) => {
-      var table = [
-        <thead key="thead">
-          <tr>
-            <th></th>
-            <th>아이템</th>
-            {isStackable && <th>개수 (EA)</th>}
-            <th>개당 가격</th>
-            <th>총 가격</th>
-            <th>어제 평균가</th>
-            <th>남은 시간</th>
-          </tr>
-        </thead>,
-      ];
+	var isStackable = itemInfo.itemType === "스태커블";
+	if (list.length === 0) {
+		return (
+			<div style={{ textAlign: "center", fontSize: "1.5rem", fontWeight: "400" }}>
+				등록된 매물이 없습니다.
+			</div>
+		);
+	} else {
+		const Table = ({ upgrade }) => {
+			var table = [
+				<thead key="thead">
+					<tr>
+						<th></th>
+						<th>아이템</th>
+						{isStackable && <th>개수 (EA)</th>}
+						<th>개당 가격</th>
+						<th>총 가격</th>
+						<th>어제 평균가</th>
+						<th>남은 시간</th>
+					</tr>
+				</thead>,
+			];
 
-      table.push(
-        <tbody key="tbody">
-          {list.map(
-            (l) =>
-              (upgrade === -1 ? true : upgrade === l.upgrade) && (
-                <tr key={`${l.auctionNo}`}>
-                  <td>
-                    <ItemImg />
-                  </td>
-                  <td>
-                    <span style={{ margin: "0px 5px" }}>
-                      {itemInfo.itemName}
-                    </span>
-                    {/* 만약 업그레이드가 가능한 카드라면 */}
-                    {itemInfo.cardInfo !== undefined &&
-                      itemInfo.cardInfo.enchant[0].explain === undefined && (
-                        <span>
-                          {l.upgrade}/{itemInfo.cardInfo.enchant.length - 1}
-                        </span>
-                      )}
-                  </td>
-                  {isStackable && <td>{l.count}</td>}
-                  <td>{l.unitPrice.toLocaleString()}</td>
-                  <td>{l.currentPrice.toLocaleString()}</td>
-                  <td>{l.averagePrice.toLocaleString()}</td>
-                  <td>
-                    {parseInt(
-                      (Date.parse(l.expireDate) - Date.now()) / 3600000
-                    )}
-                    시간
-                  </td>
-                </tr>
-              )
-          )}
-        </tbody>
-      );
-      return <table>{table}</table>;
-    };
+			table.push(
+				<tbody key="tbody">
+					{list.map(
+						l =>
+							(upgrade === -1 ? true : upgrade === l.upgrade) && (
+								<tr key={`${l.auctionNo}`}>
+									<td>
+										<ItemImg />
+									</td>
+									<td>
+										<span style={{ margin: "0px 5px" }}>{itemInfo.itemName}</span>
+										{/* 만약 업그레이드가 가능한 카드라면 */}
+										{itemInfo.cardInfo !== undefined &&
+											itemInfo.cardInfo.enchant[0].explain === undefined && (
+												<span>
+													{l.upgrade}/{itemInfo.cardInfo.enchant.length - 1}
+												</span>
+											)}
+									</td>
+									{isStackable && <td>{l.count}</td>}
+									<td>{l.unitPrice.toLocaleString()}</td>
+									<td>{l.currentPrice.toLocaleString()}</td>
+									<td>{l.averagePrice.toLocaleString()}</td>
+									<td>
+										{parseInt((Date.parse(l.expireDate) - Date.now()) / 3600000)}
+										시간
+									</td>
+								</tr>
+							),
+					)}
+				</tbody>,
+			);
+			return <table>{table}</table>;
+		};
 
-    const ButtonList = () => {
-      if (
-        itemInfo.cardInfo !== undefined &&
-        itemInfo.cardInfo.enchant[0].explain === undefined
-      ) {
-        let buttonList = [
-          <button key="all" onClick={() => setUpgrade(-1)}>
-            전체
-          </button>,
-        ];
-        for (let u = 0; u < itemInfo.cardInfo.enchant.length; u++) {
-          buttonList.push(
-            <button key={u} onClick={() => setUpgrade(u)}>
-              {u === itemInfo.cardInfo.enchant.length - 1
-                ? `풀업글`
-                : `${u}업글`}
-            </button>
-          );
-        }
-        return <div>{buttonList}</div>;
-      } else {
-        return <div />;
-      }
-    };
+		const ButtonList = () => {
+			if (itemInfo.cardInfo !== undefined && itemInfo.cardInfo.enchant[0].explain === undefined) {
+				let buttonList = [
+					<button key="all" onClick={() => setUpgrade(-1)}>
+						전체
+					</button>,
+				];
+				for (let u = 0; u < itemInfo.cardInfo.enchant.length; u++) {
+					buttonList.push(
+						<button key={u} onClick={() => setUpgrade(u)}>
+							{u === itemInfo.cardInfo.enchant.length - 1 ? `풀업글` : `${u}업글`}
+						</button>,
+					);
+				}
+				return <div>{buttonList}</div>;
+			} else {
+				return <div />;
+			}
+		};
 
-    return (
-      itemInfo.itemName !== undefined && (
-        <div>
-          <Helmet>
-            <title>{itemInfo.itemName} 검색 결과</title>
-          </Helmet>
-          {itemInfo.cardInfo !== undefined &&
-            itemInfo.cardInfo.enchant[0].explain === undefined && (
-              <div className="chooseUpgrade">
-                <ButtonList />
-              </div>
-            )}
-          <div className="auclist">
-            <Table upgrade={upgrade} />
-          </div>
-        </div>
-      )
-    );
-  }
+		return (
+			itemInfo.itemName !== undefined && (
+				<div>
+					<Helmet>
+						<title>{itemInfo.itemName} 검색 결과</title>
+					</Helmet>
+					{itemInfo.cardInfo !== undefined && itemInfo.cardInfo.enchant[0].explain === undefined && (
+						<div className="chooseUpgrade">
+							<ButtonList />
+						</div>
+					)}
+					<div className="auclist">
+						<Table upgrade={upgrade} />
+					</div>
+				</div>
+			)
+		);
+	}
 };
 
 export default AucDetail;
--- dnf/src/components/CharDetail.js
+++ dnf/src/components/CharDetail.js
@@ -7,184 +7,164 @@ import Tables from "./CharDetailTables";
 import Helmet from "react-helmet";
 
 const CharDetail = ({ match, history }) => {
-  const { server, charId } = match.params;
-  const id = charId;
-  const [equipment, setEquipment] = useState({});
-  const [avatar, setAvatar] = useState([]);
-  const [creature, setCreature] = useState([]);
-  const [buffEquipment, setBuffEquipment] = useState({});
-  const [buffAvatar, setBuffAvatar] = useState({});
-  const [buffCreature, setBuffCreature] = useState({});
-  const gotData = useRef(false);
-  const [dundamInfo, setDundamInfo] = useState([]);
-  const [tab, setTab] = useState(1);
+	const { server, charId } = match.params;
+	const id = charId;
+	const [equipment, setEquipment] = useState({});
+	const [avatar, setAvatar] = useState([]);
+	const [creature, setCreature] = useState([]);
+	const [buffEquipment, setBuffEquipment] = useState({});
+	const [buffAvatar, setBuffAvatar] = useState({});
+	const [buffCreature, setBuffCreature] = useState({});
+	const gotData = useRef(false);
+	const [dundamInfo, setDundamInfo] = useState([]);
+	const [tab, setTab] = useState(1);
 
-  if (gotData.current === false) {
-    // 장착 장비 조회
-    axios
-      .get(
-        `https://api.neople.co.kr/df/servers/${server}/characters/${id}/equip/equipment?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
-      )
-      .then((response) => {
-        setEquipment(response.data);
-      });
+	if (gotData.current === false) {
+		// 장착 장비 조회
+		axios
+			.get(
+				`https://api.neople.co.kr/df/servers/${server}/characters/${id}/equip/equipment?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`,
+			)
+			.then(response => {
+				setEquipment(response.data);
+			});
 
-    // 장착 아바타 조회
-    axios
-      .get(
-        `https://api.neople.co.kr/df/servers/${server}/characters/${id}/equip/avatar?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
-      )
-      .then((response) => {
-        setAvatar(response.data.avatar);
-      });
+		// 장착 아바타 조회
+		axios
+			.get(
+				`https://api.neople.co.kr/df/servers/${server}/characters/${id}/equip/avatar?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`,
+			)
+			.then(response => {
+				setAvatar(response.data.avatar);
+			});
 
-    // 장착 크리쳐 조회
-    axios
-      .get(
-        `https://api.neople.co.kr/df/servers/${server}/characters/${id}/equip/creature?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
-      )
-      .then((response) => {
-        setCreature(response.data.creature);
-      });
+		// 장착 크리쳐 조회
+		axios
+			.get(
+				`https://api.neople.co.kr/df/servers/${server}/characters/${id}/equip/creature?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`,
+			)
+			.then(response => {
+				setCreature(response.data.creature);
+			});
 
-    // 버프 강화 장비 조회
-    axios
-      .get(
-        `https://api.neople.co.kr/df/servers/${server}/characters/${id}/skill/buff/equip/equipment?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
-      )
-      .then((response) => {
-        setBuffEquipment(response.data.skill.buff);
-      });
+		// 버프 강화 장비 조회
+		axios
+			.get(
+				`https://api.neople.co.kr/df/servers/${server}/characters/${id}/skill/buff/equip/equipment?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`,
+			)
+			.then(response => {
+				setBuffEquipment(response.data.skill.buff);
+			});
 
-    // 버프 강화 아바타 조회
-    axios
-      .get(
-        `https://api.neople.co.kr/df/servers/${server}/characters/${id}/skill/buff/equip/avatar?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
-      )
-      .then((response) => {
-        setBuffAvatar(response.data.skill.buff);
-      });
+		// 버프 강화 아바타 조회
+		axios
+			.get(
+				`https://api.neople.co.kr/df/servers/${server}/characters/${id}/skill/buff/equip/avatar?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`,
+			)
+			.then(response => {
+				setBuffAvatar(response.data.skill.buff);
+			});
 
-    // 버프 강화 크리쳐 조회
-    axios
-      .get(
-        `https://api.neople.co.kr/df/servers/${server}/characters/${id}/skill/buff/equip/creature?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`
-      )
-      .then((response) => {
-        setBuffCreature(response.data.skill.buff);
-      });
+		// 버프 강화 크리쳐 조회
+		axios
+			.get(
+				`https://api.neople.co.kr/df/servers/${server}/characters/${id}/skill/buff/equip/creature?apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG`,
+			)
+			.then(response => {
+				setBuffCreature(response.data.skill.buff);
+			});
 
-    // 던담 정보 긁어오기
-    axios
-      .get(
-        `http://dundam.xyz/view.jsp?server=${server}&name=${equipment.characterName}&image=${id}`
-      )
-      .then((response) => {
-        const $ = cheerio.load(response.data);
-        const isBuffer = $(
-          "#Present > table > tbody > tr:nth-child(5) > td:nth-child(2)"
-        ).length;
-        if (isBuffer === 1) {
-          const list = $(
-            "#Present > table > tbody > tr:nth-child(5) > td:nth-child(2)"
-          );
-          try {
-            setDundamInfo([
-              <div key="dundamBuff">버프 : {list[0].childNodes[0].data}</div>,
-            ]);
-          } catch (TypeError) {
-            setDundamInfo([<div key="dundamBuff">버프 : 정보 없음</div>]);
-          }
-        } else {
-          let list = $(
-            "#rogen > table > tbody > tr:nth-child(13) > td:nth-child(3)"
-          );
-          try {
-            setDundamInfo([
-              <div key="dundamRogen">
-                로젠 1시 : {list[0].childNodes[0].data}
-              </div>,
-            ]);
-          } catch (TypeError) {
-            try {
-              list = $(
-                "#rogen > table > tbody > tr:nth-child(12) > td:nth-child(3)"
-              );
-              setDundamInfo([
-                <div key="dundamRogen">
-                  로젠 1시 : {list[0].childNodes[0].data}
-                </div>,
-              ]);
-            } catch (TypeError) {
-              setDundamInfo([
-                <div key="dundamRogen">로젠 1시 : 정보 없음</div>,
-              ]);
-            }
-          }
-        }
-      });
+		// 던담 정보 긁어오기
+		axios
+			.get(
+				`http://dundam.xyz/view.jsp?server=${server}&name=${equipment.characterName}&image=${id}`,
+			)
+			.then(response => {
+				const $ = cheerio.load(response.data);
+				const isBuffer = $("#Present > table > tbody > tr:nth-child(5) > td:nth-child(2)").length;
+				if (isBuffer === 1) {
+					const list = $("#Present > table > tbody > tr:nth-child(5) > td:nth-child(2)");
+					try {
+						setDundamInfo([<div key="dundamBuff">버프 : {list[0].childNodes[0].data}</div>]);
+					} catch (TypeError) {
+						setDundamInfo([<div key="dundamBuff">버프 : 정보 없음</div>]);
+					}
+				} else {
+					let list = $("#rogen > table > tbody > tr:nth-child(13) > td:nth-child(3)");
+					try {
+						setDundamInfo([<div key="dundamRogen">로젠 1시 : {list[0].childNodes[0].data}</div>]);
+					} catch (TypeError) {
+						try {
+							list = $("#rogen > table > tbody > tr:nth-child(12) > td:nth-child(3)");
+							setDundamInfo([<div key="dundamRogen">로젠 1시 : {list[0].childNodes[0].data}</div>]);
+						} catch (TypeError) {
+							setDundamInfo([<div key="dundamRogen">로젠 1시 : 정보 없음</div>]);
+						}
+					}
+				}
+			});
 
-    gotData.current = true;
-  }
+		gotData.current = true;
+	}
 
-  const onClick = (id) => {
-    // 받아온 id로 어떤 컴포넌트를 그릴지 정해야 함
-    setTab(id);
-  };
+	const onClick = id => {
+		// 받아온 id로 어떤 컴포넌트를 그릴지 정해야 함
+		setTab(id);
+	};
 
-  return (
-    equipment.characterName !== undefined && (
-      <div>
-        <Helmet>
-          <title>{equipment.characterName} 상세 정보</title>
-        </Helmet>
-        <div className="CharDetail">
-          <div className="simpleInfo">
-            {/* 사진, 직업 등 간단한 정보 */}
-            <div className="charImg">
-              <img
-                src={`https://img-api.neople.co.kr/df/servers/${server}/characters/${id}?zoom=2`}
-                alt={`${equipment.characterName}`}
-              />
-            </div>
-            <div className="info">
-              {/* 직업, 길드, 로젠 1시 딜, 버프력 등 */}
-              {equipment.adventureName}
-              <br />
-              <b>{equipment.characterName}</b>
-              <br />
-              Lv.{equipment.level} {equipment.jobGrowName}
-              <br />
-              {equipment.guildName !== null ? equipment.guildName : "길드 없음"}
-              <br />
-              {dundamInfo}
-            </div>
-          </div>
-          <div className="detailInfo">
-            {/* 나머지 전부 */}
-            <div className="tab">
-              {/* 장착 장비, 아바타, 휘장 등 바꿀 수 있게 탭 */}
-              <Tabs onClick={onClick} activeTab={tab} />
-            </div>
-            <div className="table">
-              {/* 내용 표시 (테이블 형태) */}
-              <Tables
-                id={tab}
-                history={history}
-                info={
-                  tab === 1
-                    ? equipment
-                    : tab === 2
-                    ? avatar.concat(creature)
-                    : Object.assign(buffEquipment, buffAvatar, buffCreature)
-                }
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  );
+	return (
+		equipment.characterName !== undefined && (
+			<div>
+				<Helmet>
+					<title>{equipment.characterName} 상세 정보</title>
+				</Helmet>
+				<div className="CharDetail">
+					<div className="simpleInfo">
+						{/* 사진, 직업 등 간단한 정보 */}
+						<div className="charImg">
+							<img
+								src={`https://img-api.neople.co.kr/df/servers/${server}/characters/${id}?zoom=2`}
+								alt={`${equipment.characterName}`}
+							/>
+						</div>
+						<div className="info">
+							{/* 직업, 길드, 로젠 1시 딜, 버프력 등 */}
+							{equipment.adventureName}
+							<br />
+							<b>{equipment.characterName}</b>
+							<br />
+							Lv.{equipment.level} {equipment.jobGrowName}
+							<br />
+							{equipment.guildName !== null ? equipment.guildName : "길드 없음"}
+							<br />
+							{dundamInfo}
+						</div>
+					</div>
+					<div className="detailInfo">
+						{/* 나머지 전부 */}
+						<div className="tab">
+							{/* 장착 장비, 아바타, 휘장 등 바꿀 수 있게 탭 */}
+							<Tabs onClick={onClick} activeTab={tab} />
+						</div>
+						<div className="table">
+							{/* 내용 표시 (테이블 형태) */}
+							<Tables
+								id={tab}
+								history={history}
+								info={
+									tab === 1
+										? equipment
+										: tab === 2
+										? avatar.concat(creature)
+										: Object.assign(buffEquipment, buffAvatar, buffCreature)
+								}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	);
 };
 
 export default CharDetail;
