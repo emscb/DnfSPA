75e0df0a11aff6e4b134346e2742a66b6cce74ab
Hyeok Min Kwon <khmzxc@naver.com>
2020-04-28 13:39:19 +0900

    Add store

--- dnf/src/index.js
+++ dnf/src/index.js
@@ -4,10 +4,18 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
 	<BrowserRouter>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</BrowserRouter>,
 	document.getElementById("root")
 );
