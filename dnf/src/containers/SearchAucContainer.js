374df4fff2647641bf835f8170195f5a5cf1cb8b
Hyeok Min Kwon <khmzxc@naver.com>
2020-04-28 13:38:53 +0900

    Add container

---
+++ dnf/src/containers/SearchAucContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchAuc from "../components/SearchAuc";
+import { connect } from "react-redux";
+import { addItem } from "../modules/recentSearch";
+
+const SearchAucContainer = ({ list, addItem }) => {
+	return <SearchAuc list={list} addItem={addItem}/>;
+};
+
+export default connect(state => ({ list: state.recentSearch.list }), {
+	addItem,
+},)(SearchAucContainer);
