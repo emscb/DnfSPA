42fb962cfc08718471dd1ee88af56bf0ed61d2fa
Hyeok Min Kwon <khmzxc@naver.com>
2020-10-19 15:14:52 +0900

    Updates

---
+++ precache-manifest.5472ca2e424105ccc5ef9181fe8e9ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627737a0243a825ce1d8f75a1f5bed47",
+    "url": "/DnfSPA/index.html"
+  },
+  {
+    "revision": "fe4843981d9f0b723bfc",
+    "url": "/DnfSPA/static/css/main.39e4dafc.chunk.css"
+  },
+  {
+    "revision": "846ceab10213cc7af69c",
+    "url": "/DnfSPA/static/js/2.d46137e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DnfSPA/static/js/2.d46137e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4843981d9f0b723bfc",
+    "url": "/DnfSPA/static/js/main.38334e78.chunk.js"
+  },
+  {
+    "revision": "8b08acfe7a5b3624e8a2",
+    "url": "/DnfSPA/static/js/runtime-main.d976ae1f.js"
+  }
+]);
